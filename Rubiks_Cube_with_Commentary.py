525de7cc8d33c77bb1a651ae8d79ee014903a4f2
Tanmay Garg <40921153+tanmay2004@users.noreply.github.com>
2020-09-23 12:13:21 +0530

    Add support for all OS

--- Rubiks_Cube_with_Commentary.py
+++ Rubiks_Cube_with_Commentary.py
@@ -2,12 +2,13 @@
 
 # The Random module randomly generates numbers and makes choices. This allows the program to scramble the cube and
 # decide what side to start on when solving it.
-import random
+import random, platform
 
 # The Tkinter module opens windows and places shapes, text, and colors on them. All of the graphics run through Tkinter.
 import tkinter as tk
 
-
+os_bool = (platform.system() == "Darwin")
+font_size = 18 if os_bool else 15
 
 # principal() acts as the program's main function, and it is called at the very end of the script after everything else
 # is defined. It creates a hexadecimal numbering system, runs the class, and keeps the window open until you close it.
@@ -1506,9 +1507,9 @@ class Ir():
     def Primero_Boton(self):
         color = "#A0FFFF"
         crayon = "#FFFFFF"
-        self.instrucciones = tk.Label(self.marco, font = ("Helvetica", 15), bg=color, relief="raised", bd=3, text="Instructions")
-        self.resolver = tk.Label(self.marco, font=("Helvetica", 15), bg=color, text="Solve", relief="raised", bd=3)
-        self.rapido = tk.Label(self.marco, font=("Helvetica", 15), text="Quick Solve", bg=color, relief="raised", bd=3)
+        self.instrucciones = tk.Label(self.marco, font = ("Helvetica", font_size), bg=color, relief="raised", bd=3, text="Instructions")
+        self.resolver = tk.Label(self.marco, font=("Helvetica", font_size), bg=color, text="Solve", relief="raised", bd=3)
+        self.rapido = tk.Label(self.marco, font=("Helvetica", font_size), text="Quick Solve", bg=color, relief="raised", bd=3)
         self.i_som = tk.Label(self.marco, bg="#000000")
         self.i_som.place(height=37, width=116, y=45, x=50)
         self.s_som = tk.Label(self.marco, bg="#000000")
@@ -1525,12 +1526,14 @@ class Ir():
         self.menos_alla = tk.Label(self.estuche, bg="#000000")
         self.boton_de_pagina = tk.Label(self.estuche, bg="#00FFFF", bd=2, relief="raised", text="Next")
         self.perfil = tk.Label(self.estuche, bg="#FFA000")
-        self.texto = tk.Label(self.estuche, font=("Helvetica", 14), bg=crayon, fg="#000000", justify=tk.LEFT)
         self.teclado_0 = tk.Label(self.estuche, font=("Courier", 18), bg=crayon, text="2  3")
         self.teclado_1 = tk.Label(self.estuche, font=("Courier", 18), bg=crayon, text="Q  W  E  R")
         self.teclado_2 = tk.Label(self.estuche, font=("Courier", 18), bg=crayon, text="A  S  D  F")
         self.teclado_3 = tk.Label(self.estuche, font=("Courier", 18), bg=crayon, text="X  C")
-
+        if os_bool:
+            self.texto = tk.Label(self.estuche, font=("Helvetica", 18), bg=crayon, fg="#000000", justify=tk.LEFT)
+        else:
+            self.texto = tk.Label(self.estuche, font=("Helvetica", 14), bg=crayon, fg="#000000", justify=tk.LEFT)
 
     # Assigns commands to key presses and the screen click. It is called once at the beginning and whenever the
     # instruction menu is closed.
@@ -1682,7 +1685,7 @@ class Ir():
                 [[-c, b, i], [c, b, i], [c, b, m], [-c, b, m]],
                 [[i, b, i], [m, b, i], [m, b, m], [i, b, m]]
             ]]
-        
+
         self.pagina_1 = \
 """Click the screen to start dragging the cube. Click again
 to stop. (Drag at a fairly slow pace for better results.)
@@ -1704,7 +1707,7 @@ bright colors.
 See the next page for rotating each face of the cube
 manually.\n\n"""
         self.pagina_2 = \
-"""It may help to think of the following keys as forming a
+"""It may help to think of the following keys as forming a   
 grid on your keyboard:\n\n\n\n\n\n
 2:  turns the left side up
 3:  turns the right side up
